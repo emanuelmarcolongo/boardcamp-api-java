551053996f21d6e3d7e2e78d712fc9adb451837b
emanuelmarcolongo <emanuelmarcolongo@gmail.com>
2024-01-31 14:37:14 -0300

    base project

---
+++ src/main/java/com/boardcamp/boardcampapi/BoardcampApiApplication.java
@@ -0,0 +1,13 @@
+package com.boardcamp.boardcampapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoardcampApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BoardcampApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/boardcamp/boardcampapi/BoardcampApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.boardcamp.boardcampapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BoardcampApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
